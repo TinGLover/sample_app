6335b10eada29a70e8eafabb982c28a1827e2e5a
TANGLei <tanglei@rocketmail.com>
2014-02-24 10:29:04 +0100

    Fin du layout et des routages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,14 +4,9 @@ describe PagesController do
   render_views
 
   before(:each) do
-<<<<<<< HEAD
+
      @base_title = "Simple App du Tutoriel Ruby on Rails"
-    
-=======
    
-   @base_title = "Simple App du Tutoriel Ruby on Rails"
-   
->>>>>>> filling-in-layout
   end
 
   describe "GET 'home'" do
@@ -23,12 +18,7 @@ describe PagesController do
     it "devrait avoir le bon titre" do
       get 'home'
       response.should have_selector("title",
-<<<<<<< HEAD
                                     :content => @base_title + " | Accueil")
-=======
-                        :content =>
-                         @base_title + " | Accueil")
->>>>>>> filling-in-layout
     end
   end
 
@@ -41,12 +31,7 @@ describe PagesController do
     it "devrait avoir le bon titre" do
       get 'contact'
       response.should have_selector("title",
-<<<<<<< HEAD
                                     :content => @base_title + " | Contact")
-=======
-                        :content =>
-                          @base_title + " | Contact")
->>>>>>> filling-in-layout
     end
   end
 
@@ -59,7 +44,6 @@ describe PagesController do
     it "devrait avoir le bon titre" do
       get 'about'
       response.should have_selector("title",
-<<<<<<< HEAD
                                     :content => @base_title + " | About")
     end
   end
@@ -74,10 +58,7 @@ describe PagesController do
       get 'help'
       response.should have_selector("title",
                                     :content => @base_title + " | Aide")
-=======
-                        :content =>
-                           @base_title + " | About")
->>>>>>> filling-in-layout
+
     end
   end
 end
